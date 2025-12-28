b3d284a2c509e713f9e01c5178986becdccdec92
Jalal-Taratingan <jalaltaratingan14@gmail.com>
2025-12-28 20:02:45 +0800

    Worked on with the routing/navigation

---
+++ app/src/main/java/com/example/student_planner_project/ui/navigation/Routes.kt
@@ -0,0 +1,53 @@
+// ROUTES OF THE APPLICATION
+
+package com.example.student_planner_project.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.student_planner_project.ui.viewmodels.MainViewModel
+
+@Composable
+fun Routes(mainViewModel: MainViewModel){
+    // Manages the switching of screens.
+    val navController = rememberNavController()
+
+    // Makes the display of screen reactive/dynamic for changes.
+    val semester = mainViewModel.semester.collectAsState()
+
+    // Determine what screen should be displayed after opening the application.
+    val startingScreen = if(semester.value == null){
+        Screen.Setup.route
+    }else{
+        Screen.Home.route
+    }
+
+    // Container that contains the list of the routes.
+    NavHost(navController = navController, startDestination = startingScreen){
+        composable(Screen.Setup.route){
+
+        }
+
+        composable(Screen.Home.route){
+
+        }
+
+        composable(Screen.Events.route){
+
+        }
+
+        composable(Screen.Tasks.route){
+
+        }
+
+        composable(Screen.Notepad.route){
+
+        }
+
+        composable(Screen.StudyTimer.route){
+
+        }
+    }
+}

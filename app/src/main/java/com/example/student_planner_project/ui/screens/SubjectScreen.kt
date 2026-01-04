0972e11f5c70ad02b69ddffe35912f681e3e639c
Jalal-Taratingan <jalaltaratingan14@gmail.com>
2026-01-04 14:36:53 +0800

    built another feature for subject screen where it displays the task and notes of the selected subject

--- app/src/main/java/com/example/student_planner_project/ui/screens/SubjectScreen.kt
+++ app/src/main/java/com/example/student_planner_project/ui/screens/SubjectScreen.kt
@@ -2,6 +2,7 @@
 
 package com.example.student_planner_project.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,18 +10,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -39,11 +45,14 @@ fun SubjectScreen(mainViewModel: MainViewModel){
     val semester = mainViewModel.semester.collectAsState()
     val currentSemester = semester.value
     val pressedAdd = remember {mutableStateOf(false)}
+    val selectedSubject = remember {mutableStateOf<Subject?>(null)}
+    val subject = selectedSubject.value
+
 
     Scaffold(
         // Add Button
         floatingActionButton =  {
-            if(pressedAdd.value == false && currentSemester != null) {
+            if(pressedAdd.value == false && currentSemester != null && subject == null) {
                 FloatingActionButton(onClick = { pressedAdd.value = true }){
                     Text(text = "+")
                 }
@@ -51,31 +60,56 @@ fun SubjectScreen(mainViewModel: MainViewModel){
         }
     ){ paddingValues ->
         Column(modifier = Modifier.fillMaxSize().padding(paddingValues).padding(16.dp)){
-            Text(text = "Subjects", style = MaterialTheme.typography.headlineLarge,fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth().padding(bottom = 15.dp))
-
             Box(modifier = Modifier.weight(1f)) {
+                // Display the details of the selected subject
+                if(subject != null){
+                    DisplayDetails(subject){
+                        selectedSubject.value = null
+                    }
+
                 // Add a subject
-                if (pressedAdd.value == true) {
-                    NewSubject(mainViewModel, pressedAdd)
+                } else if (pressedAdd.value == true) {
+                    NewSubject(mainViewModel, pressedAdd){
+                        pressedAdd.value = false
+                    }
 
                 } else {
+                    Column {
+                        Text(
+                            text = "Subjects",
+                            style = MaterialTheme.typography.headlineLarge,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.fillMaxWidth().padding(bottom = 15.dp)
+                        )
 
-                    // No semester
-                    if (currentSemester == null) {
-                        Text(text = "Create a semester to add subjects.", modifier = Modifier.align(Alignment.Center))
+                        // No semester
+                        if (currentSemester == null) {
+                            Text(
+                                text = "Create a semester to add subjects.",
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            )
 
-                        // No existing subjects
-                    } else if (currentSemester.subjects.isEmpty()) {
-                        Text(text = "No existing subjects.", modifier = Modifier.align(Alignment.Center))
+                            // No existing subjects
+                        } else if (currentSemester.subjects.isEmpty()) {
+                            Text(
+                                text = "No existing subjects.",
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            )
 
-                        // Has semester and subjects
-                    } else {
-                        LazyColumn(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalArrangement = Arrangement.spacedBy(10.dp)
-                        ) {
-                            items(currentSemester.subjects) { subject ->
-                                DisplaySubject(mainViewModel, subject)
+                            // Has semester and subjects
+                        } else {
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.spacedBy(10.dp)
+                            ) {
+                                items(currentSemester.subjects) { subject ->
+                                    DisplaySubject(
+                                        mainViewModel,
+                                        subject,
+                                        pressed = { clickedSubject ->
+                                            selectedSubject.value = clickedSubject
+                                        })
+                                }
                             }
                         }
                     }
@@ -89,10 +123,22 @@ fun SubjectScreen(mainViewModel: MainViewModel){
 
 // Adds a subject
 @Composable
-fun NewSubject(mainViewModel: MainViewModel, pressedAdd: MutableState<Boolean>){
+fun NewSubject(mainViewModel: MainViewModel, pressedAdd: MutableState<Boolean>, pressedBack: () -> Unit){
     val subjectName = remember { mutableStateOf("") }
     val subjectProfessor = remember { mutableStateOf("")}
     val subjectSchedule = remember { mutableStateOf("")}
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        IconButton(
+            onClick = { pressedBack() },
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBackIos,
+                contentDescription = "Back",
+            )
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -139,11 +185,11 @@ fun NewSubject(mainViewModel: MainViewModel, pressedAdd: MutableState<Boolean>){
     }
 }
 
-// Displays the subject details
+// Displays the subject
 @Composable
-fun DisplaySubject(mainViewModel: MainViewModel, subject: Subject){
+fun DisplaySubject(mainViewModel: MainViewModel, subject: Subject, pressed: (Subject) -> Unit){
     Card(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth().clickable{pressed(subject)}
     ){
         Row(
             modifier = Modifier.padding(10.dp),
@@ -167,6 +213,63 @@ fun DisplaySubject(mainViewModel: MainViewModel, subject: Subject){
                     imageVector = Icons.Default.Delete,
                     contentDescription = "Delete Subject"
                 )
+            }
+        }
+    }
+}
+
+// Displays the details of the selected subject
+@Composable
+fun DisplayDetails(subject: Subject, pressedBack: () -> Unit){
+    val selectedTab = remember {mutableStateOf(0)}
+    val tabs = listOf("Tasks", "Notes")
+
+    Column(){
+        Box(modifier = Modifier.fillMaxWidth()) {
+            IconButton(
+                onClick = { pressedBack() }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIos,
+                    contentDescription = "Back",
+                )
+            }
+
+            Text(
+                text = "Subjects",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.Center)
+            )
+
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            color = MaterialTheme.colorScheme.outlineVariant,
+            thickness = 1.dp
+        )
+
+        Text(text = subject.name, style = MaterialTheme.typography.headlineLarge,fontWeight = FontWeight.SemiBold, modifier = Modifier.fillMaxWidth())
+        Text(text = subject.professor, style = MaterialTheme.typography.bodyMedium)
+        Text(text = subject.schedule, style = MaterialTheme.typography.bodyMedium)
+
+        TabRow(
+            selectedTabIndex = selectedTab.value,
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = selectedTab.value == index,
+                    text = {Text(text = title)},
+                    onClick = {selectedTab.value = index}
+                )
+            }
+        }
+
+        Box(){
+            if(selectedTab.value == 0){
+                Text(text = "Tasks")
+            }else{
+                Text(text = "Notes")
             }
         }
     }
